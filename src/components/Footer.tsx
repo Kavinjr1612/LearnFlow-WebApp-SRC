9dbacc744e07574097cc972c42013c1976794424
Kavinesh S R <kavinjr1612@gmail.com>
2025-11-24 09:27:56 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import { Link } from 'react-router-dom';
+import { GraduationCap, Facebook, Twitter, Instagram, Linkedin, Mail } from 'lucide-react';
+export function Footer() {
+  return <footer className="border-t bg-card mt-auto">
+      
+    </footer>;
+}
