faaa334f872a1de782bafcabb38f73d8b2d623e3
Kavinesh S R <kavinjr1612@gmail.com>
2025-11-24 04:26:25 +0000

    Change

--- lib/appConfig.ts
+++
@@ -1,6 +0,0 @@
-// Update this URL after uploading your APK to Supabase Storage or other hosting
-export const APP_CONFIG = {
-  androidApkUrl: 'https://wjlybfshxpamzngalghv.supabase.co/storage/v1/object/public/Apk/app-debug.apk',
-  appVersion: '1.0.0',
-  apkFileName: 'app-debug.apk'
-};
